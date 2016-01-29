a5649123a87dc1c9cc99d5e77175b89292497d78
Kolesnikova Kate <ek@trinitymobile.ru>
2016-01-29 11:36:09 +0300

    Добавлен дата маппинг

---
+++ ulyanovsk/Mapping/DataMapping.h
@@ -0,0 +1,16 @@
+//
+//  DataMapping.h
+//  ulyanovsk
+//
+//  Created by Kolesnikova Ekaterina on 28/01/16.
+//  Copyright © 2016 Ekaterina Kolesnikova. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <FastEasyMapping/FastEasyMapping.h>
+
+@interface DataMapping : NSObject
+
++ (FEMManagedObjectMapping *)placeMappingForEntityName:(NSString *)entityName;
+
+@end
